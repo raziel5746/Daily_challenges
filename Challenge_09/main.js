95bee035aec867e424150c789a25a3da3f157713
Raziel <raziel5746@gmail.com>
2019-11-09 21:11:18 +0200

    challenge 9 update

--- Challenge_09/main.js
+++ Challenge_09/main.js
@@ -1,44 +1,46 @@
-let stringList = ["Hello","World","in","a","frame"];
 
-let longestWord = 0;
-
-let framed = document.getElementById("framed");
-
-let newStringList = [];
-
-let finalString = "";
-
-for (let i = 0; i < stringList.length; i++) {
-    if (stringList[i].length > longestWord) {
-        longestWord = stringList[i].length;
+function start() {
+    let mySentence = prompt("Write a sentence:");
+    let wordArray = mySentence.split(" ");
+    let longestWord = 0;
+    let framed = document.getElementById("framed");
+    let newWordArray = [];
+    let finalString = "";
+    
+    for (let i = 0; i < wordArray.length; i++) {
+        if (wordArray[i].length > longestWord) {
+            longestWord = wordArray[i].length;
+        };
     };
-};
-
-for (let i = 0; i < stringList.length; i++) {
-    newStringList[i] = "*" + "x" + stringList[i];
-    while (newStringList[i].length < (longestWord + 3)) {
-        newStringList[i] += "x";
+    
+    for (let i = 0; i < wordArray.length; i++) {
+        newWordArray[i] = "*" + "x" + wordArray[i];
+        while (newWordArray[i].length <= (longestWord + 2)) {
+            newWordArray[i] += "x";
+        };
+    
+        newWordArray[i] += "*<br>";
+    };
+    
+    for (let i = 0; i < newWordArray.length; i++) {
+        finalString += newWordArray[i];
+    };
+    
+    while (finalString.indexOf("x") > 0) {
+        finalString = finalString.replace("x", "&nbsp");
     };
 
-    newStringList[i] += "*<br>";
+    console.log(finalString)
+    
+    let starsLine = "";
+    
+    for (i = 0; i <= longestWord + 3; i++) {
+        starsLine += "*";
+    };
+    
+    starsLine += "<br>";
+    
+    finalString = starsLine + finalString + starsLine;
+    
+    framed.innerHTML = finalString;
 };
-
-for (let i = 0; i < newStringList.length; i++) {
-    finalString += newStringList[i];
-};
-
-while (finalString.indexOf("x") > 0) {
-    finalString = finalString.replace("x", "&nbsp");
-};
-
-let starsLine = "";
-
-for (i = 0; i <= longestWord + 3; i++) {
-    starsLine += "*";
-};
-
-starsLine += "<br>";
-
-finalString = starsLine + finalString + starsLine;
-
-framed.innerHTML = finalString;
